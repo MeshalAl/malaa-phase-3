023a9de67cd1dfc78b7de8f5016fb92e77f93d7c
MeshalAl <meshal@alanazi.io>
2023-08-23 14:34:01 +0300

    added environ for host and ports.

--- phase_3/investor_bulletin/event_subscriber/main.py
+++ phase_3/investor_bulletin/event_subscriber/main.py
@@ -1,23 +1,26 @@
 from pika import BlockingConnection, ConnectionParameters
+from os import environ
+from dotenv import load_dotenv
+from _config.logger_config import logger
+
 # Create a connection object to start consuming events
 
 def init_subscriber():
+  load_dotenv()
+  broker = environ.get("BROKER")
+  broker_port = environ.get("BROKER_PORT")
 
-  connection =  BlockingConnection(ConnectionParameters(host="localhost", port=5672))
+  connection =  BlockingConnection(ConnectionParameters(host=broker, port=broker_port))
   channel = connection.channel()
   channel.queue_declare(queue="THRESHOLD_ALERTS_QUEUE")
   return channel
 
 def on_event(ch, method, properties, body):
-  print(f"Alert received: {body}")
+  logger.info(f"Alert received: {body}")
 
 if __name__ == "__main__":
-    import os, sys
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    root_dir = os.path.dirname(current_dir)
-    sys.path.append(root_dir)
 
     subscriber = init_subscriber()
-    subscriber.basic_consume(queue="THRESHOLD_ALERTS_QUEUE", on_message_callback=on_event, auto_ack=True)
+    subscriber.basic_consume(queue="THRESHOLD_ALERTS_QUEUE", on_message_callback=on_event)
     subscriber.start_consuming()
     subscriber.close()
