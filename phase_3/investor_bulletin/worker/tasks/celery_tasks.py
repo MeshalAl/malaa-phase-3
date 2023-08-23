2a9b0f6ef73e494ae19cb3b1b126c33b32485e0b
MeshalAl <meshal@alanazi.io>
2023-08-23 14:40:45 +0300

    changed publish message from function to api call

--- phase_3/investor_bulletin/worker/tasks/celery_tasks.py
+++ phase_3/investor_bulletin/worker/tasks/celery_tasks.py
@@ -1,52 +1,70 @@
 import requests
-from worker.app import app
-from core.messaging import init_broker, publish_message
+from app import app
 from os import environ
-from resources.alerts.alert_schema import AlertCreate
+from malaa_schema.alert_schema import AlertCreate
+from _config.logger_config import logger
 
+api = environ.get('BACKEND_URL')
 
-api = environ.get('API_URL')
-
-@app.task
 def fetch_market_data():
     url = f"{api}/{environ.get('MARKET_PRICES')}"
-    market_data = requests.get(url).json()
+    market_data = requests.get(url).json().get("price_list")
     if market_data:
         return market_data
-    return None
+    return []
 
-@app.task
 def fetch_alert_rules():
-    url = f"{api}/{environ.get('ALERT')}/{environ.get('ALERT_RULES')}"
+    url = f"{api}/{environ.get('ALERTS_ROUTE')}/{environ.get('ALERT_RULES_ROUTE')}"
     alert_rules = requests.get(url).json()
     if alert_rules:
         return alert_rules
-    return None
+    return []
 
-@app.task
 def find_matching_rules(market_data, alert_rules):
-    return [
-        {"name": rule["name"], "threshold_price": price["price"], "symbol": price["symbol"]}
-        for rule in alert_rules
-        for price in market_data
-        if price["price"] < rule["threshold_price"] and price["symbol"] == rule["symbol"]
-    ]
+    matches = []
+
+    for rule in alert_rules:
+        for price in market_data:
+            if price["price"] < rule["threshold_price"] and price["symbol"] == rule["symbol"]:
+                match = {
+                    "name": rule["name"],
+                    "threshold_price": price["price"],
+                    "symbol": price["symbol"]
+                }
+                matches.append(match)
+
+    return matches
+
+def publish_alerts(matching_alert: AlertCreate, session: requests.Session):
+
+    url = f"{api}/{environ.get('MESSAGING_ROUTE')}/"
+
+    payload = matching_alert.model_dump_json()
+    headers = {'content-type': 'application/json'}
+    try:
+        response = session.post(url, data=payload, headers=headers)
+        response.raise_for_status()
+        logger.info(f"Message published: {payload}")
+    except Exception as e:
+        logger.warning(f"Attempt failed: {e}")
 
 @app.task
 def publish_alerts_task():
-    print('running: publish_alerts_task')
 
-    market_data = fetch_market_data.run().get()
-    alert_rules = fetch_alert_rules.run().get()
+    market_data = fetch_market_data()
+    alert_rules = fetch_alert_rules()
 
-    print(market_data, alert_rules)
+    session = requests.Session()
 
-    broker = init_broker()
+    matches = find_matching_rules(market_data, alert_rules) or []
 
-    for match in find_matching_rules.run(market_data, alert_rules).get():
-        publish_message(broker, AlertCreate(**match))
-    publish_message(broker, AlertCreate(name="Published Alert", threshold_price=100.01, symbol="CELERY-LAST"))
+    if matches:
+        for match in matches:
+            publish_alerts(AlertCreate(**match), session)
+        logger.info(f"Published {len(matches)} alerts")
+    else:
+        logger.info("No alerts published")
 
 @app.task
 def ping():
-    return 'pong'
+    logger.info("Pong")
